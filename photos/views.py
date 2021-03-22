6e3c1a9d231218023c5bb118fb46bfa7629b7b89
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-22 12:07:12 +0300

    addition of search function

--- photos/views.py
+++ photos/views.py
@@ -10,7 +10,19 @@ from .models import Image
 
 def photos(request):
     images = Image.objects.all()
-    # locations = Location.objects.all()
+    locations = Location.objects.all()
     return render(request, 'welcome.html',{'image':images[::1]})
 
+def search_image(request):
+    title ='search'
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'image_category' in request.Get and request.Get['image_category']:
+        search_term = request.Get.get('image_category')
+        found_results = Image.search_by_category(search_term)
+        messge =f"{search_term}"
+        print(search_term)
+        print(found_results)
+
+        return render(request, 'search.html',{'title':title,'images':found_results, 'message':message, 'categories':categories, 'locations':locations})
 
