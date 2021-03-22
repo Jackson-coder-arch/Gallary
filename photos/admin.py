034975c1121f793d057bbfe3aa3ffb33c792f1e0
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-22 12:17:22 +0300

    add of navbar

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category,Location,Image
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    filter_horizontal =('image',)
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image)
--- photos/urls.py
+++ photos/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.photos,name = 'welcome'),
+    url(r'^search/',views.search_image, name = 'search_image'),
+    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
    
-
-
-
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
