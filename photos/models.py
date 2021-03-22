81eaa6d5c1c1bacb7a3698a4ec8ea70145fb3fe8
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-22 12:04:16 +0300

    addition of class category

--- photos/models.py
+++ photos/models.py
@@ -40,8 +40,6 @@ class Image(models.Model):
         ordering = ['name']
 
 
-
-
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
@@ -63,3 +61,21 @@ class Location(models.Model):
 
     def delete_location(self):
         self.delete()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self, update):
+        self.name = update
+        self.save()
+
