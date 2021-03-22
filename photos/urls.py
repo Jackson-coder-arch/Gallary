a9c19777f6d7bba7f338d93fceb8aee072cef5db
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-22 12:03:17 +0300

    addition of class location

--- photos/urls.py
+++ photos/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.photos_of_day,name='photosToday')
+    url(r'^$',views.photos,name = 'welcome'),
+   
+
 
 
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,32 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
+from .models import Image
 
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'Welcome.html')
+# def welcome(request):
+#     return render(request, 'Welcome.html')
 
-def photos_of_day(request):
-    date = dt.date.today()
-   # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+def photos(request):
+    images = Image.objects.all()
+    # locations = Location.objects.all()
+    return render(request, 'welcome.html',{'image':images[::1]})
 
-def convert_dates(dates):
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
